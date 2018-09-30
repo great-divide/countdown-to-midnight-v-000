42bc5ff3843596ed9dd927f3f9f2bb0624c1e845
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 05:10:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,11 @@ def countdown(number)
 end
 
 def countdown_with_sleep
+    while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1.second)
+  end
+  
+  "HAPPY NEW YEAR!"
 end
